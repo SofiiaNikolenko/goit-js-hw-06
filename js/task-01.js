8a6a076b7fec2d61003c99b2e10eace24eca1d8f
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-03-19 19:45:38 +0100

    1 task completed

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,7 @@ console.log("Number of categories:",allLiItem.length);
 allLiItem.forEach(item => {
     const itemH2 = item.querySelector(".item > h2");
     console.log("Category:", itemH2.textContent);
+    
     const allLiInItem = item.querySelectorAll(".item > ul > li");
     console.log("Elements:",allLiInItem.length);
 });
