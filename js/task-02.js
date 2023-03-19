5fd0834d2bb317af3679e309154550a7bb6405e3
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-03-19 20:08:42 +0100

    2 task completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const Ulid = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const ulElement = document.createElement("li");
+  ulElement.textContent = ingredient;
+  ulElement.classList.add("item");
+  Ulid.append(ulElement);
+});
