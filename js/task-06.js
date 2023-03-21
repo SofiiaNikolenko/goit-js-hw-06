77cb8e0389a378d3b46dd24ad8ec706c78ac846f
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-03-21 18:11:35 +0100

    6 task completed

--- js/task-06.js
+++ js/task-06.js
@@ -3,11 +3,9 @@ const lengthInput = nameInput.dataset.length;
 
 const inputEvent = (event) => { 
     if (event.currentTarget.value.length >= lengthInput) {
-        console.log("hello");
         nameInput.classList.remove("invalid");
         nameInput.classList.add("valid");
     } else {
-        console.log("no hello");
         nameInput.classList.remove("valid");
         nameInput.classList.add("invalid");
     }
