45fa88a17e093d68d6971460a880caaf7da41f09
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2023-03-21 18:42:52 +0100

    8 task completed

---
+++ js/task-08.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('.login-form');
+
+function formSubmit(event){
+    event.preventDefault();
+
+    const { elements: { email, password } } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        alert("Fill in all data!");
+    }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+};
+
+form.addEventListener("submit", formSubmit);
